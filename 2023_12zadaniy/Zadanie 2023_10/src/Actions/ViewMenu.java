6a2119a59d2a8492fba4c0f2dce5f7e3d88a18e1
Tanya <shubynina22@gmail.com>
2023-11-18 21:11:11 +0300

    1

---
+++ 2023_12zadaniy/Zadanie 2023_10/src/Actions/ViewMenu.java
@@ -0,0 +1,13 @@
+package Actions;
+
+public class ViewMenu {
+    public void viewMenu(){
+        System.out.println("""
+                ``Menu``
+                1 dfvdfv
+                2 rserf
+                3 ytntf
+                4 rert
+                """);
+    }
+}
